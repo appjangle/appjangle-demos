ab9aa2078de8d2f53e86ba410a8c3e00eb895a54
mxro <mxro@nowhere.com>
2013-01-07 17:26:25 +1300

    building first example, instant data

--- src/main/js/demo_instant_data.js
+++ src/main/js/demo_instant_data.js
@@ -1,4 +1,22 @@
-<!-- one.upload https://u1.linnk.it/qc8sbw/usr/apps/textsync/files/demo_instant_data_js -->
+// <!-- one.upload https://u1.linnk.it/qc8sbw/usr/apps/textsync/files/demo_instant_data_js -->
 
+window.Appjangle = window.Appjangle || {};
 
-<!-- one.end -->
+(function(Appjangle, $) {
+
+	Appjangle.ui = Appjangle.ui || {};
+	/*
+	 * Returns an aj login object.
+	 * 
+	 */
+	Appjangle.demos.instantData = function(params) {
+
+		return {
+
+		};
+
+	};
+
+}(window.Appjangle, window.jQuery));
+
+// <!-- one.end -->
