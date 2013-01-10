abd333d95c0451ee9594b71a5671ccf2944fbe40
mxro <mxro@nowhere.com>
2013-01-11 12:08:34 +1300

    updating to revised demo 1 source code

--- src/main/js/demo_transparent_cloud_persistence.js
+++ src/main/js/demo_transparent_cloud_persistence.js
@@ -7,90 +7,101 @@ window.Appjangle = window.Appjangle || {};
 (function(Appjangle, $) {
 	// constants
 	var postWrapper = "<li class='media'></li>",
-	    postType = "http://slicnet.com/seed1/seed1/3/6/5/2/h/sd/aPost1";
+	    postType;
 
 	Appjangle.demos = Appjangle.demos || {};
 	
-	Appjangle.demos.initTransparentCloudPersistenceDemo = function(params) {
-		var wrapper, Nextweb, server, session, posts, aPost;
+	Appjangle.demos.initInstantDataDemo = function(params) {
+		var wrapper, Nextweb, server, session, posts, aPost, demo;
 
+		demo =  {};
+		
 		// jQuery element wrapping all required HTML
 		wrapper = params.wrapper;
 		Nextweb= params.engine;
-	
+		
+		// local db server to manage data
+		server = Nextweb.startServer(12322);
 		session = Nextweb.createSession();
 
-		// request for data node to store all posts
-		if (param.node === null) {
-			posts = session.seed();
+		// load or request for data node to store all posts
+		if (!params.node) {
+			posts = session.seed("local");
 		} else {
 			posts = session.node(params.node);
 		}
 		
-		// define data type for posts
+		// load data type for posts
+		postType = "http://slicnet.com/seed1/seed1/3/6/5/2/h/sd/aPost1";
 		aPost = session.node(postType);
 		
 		// resolving request for data node
 		posts.get(function(posts) {
-			
-			// init UI
-			(function() {
-				var createItem, updatePosts, updateTotal;
-				createItem = function() {
-					var postContent;
-					postContent= $(".postTemplate", wrapper).clone();
-					postContent.removeClass("hide");
-					postContent.removeClass("postTemplate");
-					return $(postWrapper).append(postContent);
-				};
-				
-				updatePosts = function() {
-					var i, item;
-					
-					posts.selectAll(aPost).get(function(postsList) {
-						$(".postList", wrapper).empty();
-						for (i=postsList.values().length-1; i>=0; i--) {
-							item = createItem();
-							$(".postList", wrapper).append(item);
-							
-							$(".postText", item).text(postsList.values()[i]);
-							
-						}
-						
-						if (postsList.size() == 0) {
-							$(".noPostsYet", wrapper).show();
-						} else {
-							$(".noPostsYet", wrapper).hide();
-						}
-					});
-				};
-				
-				updateTotal = function() {
-					posts.selectAll(aPost).get(function(postsList) {
-						$(".totalPosts", wrapper).text(postsList.size());
-					});
-				};
-				
-				$(".postButton", wrapper).click(function(evt) {
-					evt.preventDefault();
-					
-					posts.append($(".postInput", wrapper).val()).append(aPost);
-					$(".postInput", wrapper).val("");
-					session.commit().get(function() {
-						
-						updatePosts();
-						updateTotal();
-						
-					});
-					
-				});
-				
-				updatePosts();
-				updateTotal();
-				wrapper.show();
-			}());
+			// when data node loaded, show ui
+			demo.initUi();
 		});
 
+		// submit a new post
+		demo.submitPost = function() {
+			posts.append($(".postInput", wrapper).val()).append(aPost);
+			$(".postInput", wrapper).val("");
+			session.commit().get(function() {
+				
+				demo.updatePosts();
+				demo.updateTotal();
+				
+			});
+		};
+		
+		demo.updateTotal = function() {
+			posts.selectAll(aPost).get(function(postsList) {
+				$(".totalPosts", wrapper).text(postsList.size());
+			});
+		};
+		
+		demo.updatePosts = function() {
+			var i, item;
+			
+			posts.selectAll(aPost).get(function(postsList) {
+				$(".postList", wrapper).empty();
+				for (i=postsList.values().length-1; i>=0; i--) {
+					item = demo.createItem();
+					$(".postList", wrapper).append(item);
+					
+					$(".postText", item).text(postsList.values()[i]);
+				}
+				
+				if (postsList.size() == 0) {
+					$(".noPostsYet", wrapper).show();
+				} else {
+					$(".noPostsYet", wrapper).hide();
+				}
+			});
+		};
+		
+		
+		demo.initUi = function() {
+			var createItem, updatePosts, updateTotal;
+
+			$(".postButton", wrapper).click(function(evt) {
+				evt.preventDefault();
+				
+				demo.submitPost();
+				
+			});
+			demo.updateTotal();
+			demo.updatePosts();
+			wrapper.show();
+		};
+		
+		demo.createItem = function() {
+			var postContent;
+			postContent= $(".postTemplate", wrapper).clone();
+			postContent.removeClass("hide");
+			postContent.removeClass("postTemplate");
+			return $(postWrapper).append(postContent);
+		};
+		
 		return {
 			wrapper : wrapper,
 			shutdown : function() {
