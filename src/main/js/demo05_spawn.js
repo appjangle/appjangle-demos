cc68591744be7a9febe52d70ab6f558f81192ebc
Max <Max@Max-PC>
2013-02-05 10:35:46 +1300

    adding more client variants

--- src/main/js/demo05_spawn.js
+++ src/main/js/demo05_spawn.js
@@ -4,12 +4,10 @@
 
 window.Appjangle = window.Appjangle || {};
 
-
-
 (function(Appjangle, $) {
 
 	Appjangle.demos = Appjangle.demos || {};
-	
+
 	Appjangle.demos.initDemo05Spawn = function(params) {
 		var elem = params.wrapper;
 		var postsUrl = params.postsUrl;
@@ -21,7 +19,9 @@ window.Appjangle = window.Appjangle || {};
 						"href",
 						appUrl
 								+ "#"
-								+ postsUrl + "&"+postsSecret
+								+ postsUrl
+								+ "&"
+								+ postsSecret
 								+ "&Caesar&http://docs.google.com/drawings/d/1nL9LVuDNjakiFW9YL5t1QOMtUgn2bVIl4OAXpPS6pMw/pub?w=50");
 
 		$(".spawnCleopetra", elem)
@@ -29,9 +29,31 @@ window.Appjangle = window.Appjangle || {};
 						"href",
 						appUrl
 								+ "#"
-								+ postsUrl + "&" +postsSecret
+								+ postsUrl
+								+ "&"
+								+ postsSecret
 								+ "&Cleopetra&http://docs.google.com/drawings/d/1ITfQfEuslX7P0Mj0ZYhCzKFUNdmU0gUXbJcEI_mz0oE/pub?w=50");
-		
+
+		$(".spawnSocrates", elem)
+				.attr(
+						"href",
+						appUrl
+								+ "#"
+								+ postsUrl
+								+ "&"
+								+ postsSecret
+								+ "&Socrates&http://docs.google.com/drawings/d/1kqVqej6c6R6PGpox-STQEc9O6fZO9zGicL5S6mmBkdQ/pub?w=50");
+
+		$(".spawnLincoln", elem)
+				.attr(
+						"href",
+						appUrl
+								+ "#"
+								+ postsUrl
+								+ "&"
+								+ postsSecret
+								+ "&Lincoln&http://docs.google.com/drawings/d/1VyBRCBo2OOOcBVOmiRLP8r-ZPvva-VolSenXdnni4VM/pub?w=50");
+
 		elem.show();
 
 	};
