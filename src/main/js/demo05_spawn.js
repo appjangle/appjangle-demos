5cac974b4583e9d31e88549c4a64ffabc7436d8b
mxro <mxro@nowhere.com>
2013-02-04 16:26:59 +1300

    reworking sync showcase, working on allowing to spawn mutiple clients

---
+++ src/main/js/demo05_spawn.js
@@ -0,0 +1,36 @@
+// <!-- one.upload https://u1.linnk.it/qc8sbw/usr/apps/textsync/files/demo05_spawn -->
+
+/*global window: false */
+
+window.Appjangle = window.Appjangle || {};
+
+(function(Appjangle, $) {
+
+	Appjangle.initDemo05Spawn = function(params) {
+		var elem = params.elem;
+		var postsUrl = params.postsUrl;
+		var appUrl = "http://u1.linnk.it/qc8sbw/usr/apps/textsync/files/demo-collaboration_app.value.html";
+
+		$(".spawnCaesar", elem)
+				.attr(
+						"href",
+						appUrl
+								+ "#"
+								+ postsUrl
+								+ "&Caesar&http://docs.google.com/drawings/d/1nL9LVuDNjakiFW9YL5t1QOMtUgn2bVIl4OAXpPS6pMw/pub?w=50");
+
+		$(".spawnCleopetra", elem)
+				.attr(
+						"href",
+						appUrl
+								+ "#"
+								+ postsUrl
+								+ "&Cleopetra&http://docs.google.com/drawings/d/1ITfQfEuslX7P0Mj0ZYhCzKFUNdmU0gUXbJcEI_mz0oE/pub?w=50");
+		
+		
+
+	};
+
+}());
+
+// <!-- one.end -->
