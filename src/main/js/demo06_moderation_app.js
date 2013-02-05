e01b497407a2cbf88914c5f22a762f00840cc265
Max <Max@Max-PC>
2013-02-05 18:44:05 +1300

    fixing dependencies between files for demo 06

---
+++ src/main/js/demo06_moderation_app.js
@@ -0,0 +1,11 @@
+// <!-- one.upload https://u1.linnk.it/qc8sbw/usr/apps/textsync/files/demo05_moderation_app_js -->
+
+remarkableType = "http://slicnet.com/seed1/seed1/4/5/0/2/h/sd/remarkable";
+		aRemarkablePost = session.node(remarkableType);
+		
+moderationType= "http://slicnet.com/seed1/seed1/4/5/0/2/h/sd/moderation";
+aModeration = session.node(moderationType); 
+
+$(".markAsRemarkable", item)
+
+// <!-- one.end -->
