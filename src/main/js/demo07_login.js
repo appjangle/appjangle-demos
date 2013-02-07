2e307792b6ca29c5d62b89bda8b60844389780c3
Max <Max@Max-PC>
2013-02-07 15:48:33 +1300

    working on demo login page

--- src/main/js/demo07_login.js
+++ src/main/js/demo07_login.js
@@ -1,4 +1,4 @@
-// <!-- one.createPublic demo07_login -->
+// <!-- one.upload https://u1.linnk.it/qc8sbw/usr/apps/textsync/files/demo07_login1 -->
 
 /*global window: false */
 
@@ -10,179 +10,16 @@ window.Appjangle = window.Appjangle || {};
 	Appjangle.demos = Appjangle.demos || {};
 
 	Appjangle.demos.initLoginDemo = function(params) {
-		var wrapper, avatar, userName, Nextweb, server, session, posts, postType, aPost, avatarType, anAvatar, userNameType,
-		remarkableType, remarkable, moderationType, moderation,
-		aUserName, demo, monitor;
+		var wrapper = params.wrapper;
+		var posts = params.posts;
+		var Nextweb = params.engine;
 
-		demo = {};
-
-		avatar = params.avatar;
-		userName = params.userName;
-		// jQuery element wrapping all required HTML
-		wrapper = params.wrapper;
-		Nextweb = params.engine;
-		session = params.posts.getSession();
-		posts = params.posts;
-		moderations = params.moderations;
 		
-		monitor = null;
-
-		postType = "http://slicnet.com/seed1/seed1/3/6/5/2/h/sd/aPost1";
-		aPost = session.node(postType);
-		aPost.get();
-
-		avatarType = "http://slicnet.com/seed1/seed1/3/9/1/3/h/sd/anAvatar";
-		anAvatar = session.node(avatarType);
-
-		userNameType = "http://slicnet.com/seed1/seed1/3/9/2/3/h/sd/userName";
-		aUserName = session.node(userNameType);
-
-		remarkableType = "http://slicnet.com/seed1/seed1/4/5/0/2/h/sd/remarkable";
-		aRemarkablePost = session.node(remarkableType);
 		
-		moderationType= "http://slicnet.com/seed1/seed1/4/5/0/2/h/sd/moderation";
-		aModeration = session.node(moderationType); 
-		
-		var updatePending = false;
-		demo.update = function() {
-			if (updatePending) {
-				return;
-			}
-			updatePending = true;
-			demo.updatePosts();
-			demo.updateTotal();
-			updatePending = false;
-		};
-
-		demo.updateTotal = function() {
-			posts.selectAll(aPost).get(function(postsList) {
-				$(".totalPosts", wrapper).text(postsList.size());
-			});
-		};
-
-		demo.updatePosts = function() {
-			var post;
-
-			posts.selectAll(aPost).get(function(postsList) {
-				var i, item;
-
-				$(".postList", wrapper).empty();
-
-				for (i = postsList.nodes().length - 1; i >= 0; i--) {
-					post = postsList.nodes()[i];
-
-					demo.renderPost(post);
-				}
-
-				if (postsList.size() === 0) {
-					$(".noPostsYet", wrapper).show();
-				} else {
-					$(".noPostsYet", wrapper).hide();
-				}
-
-			});
-		};
-
-		demo.renderPost = function(post) {
-			var item, userNameNode, avatarNode;
-			item = demo.createItem();
-
-			$(".postList", wrapper).append(item);
-			$(".postText", item).text(post.value());
-
-			userNameNode = post.select(aUserName);
-			userNameNode.catchUndefined(ignore);
-			userNameNode.get(function(userNameNode) {
-				$(".postAuthor", item).text(userNameNode.value());
-			});
-			
-			avatarNode = post.select(anAvatar);
-			avatarNode.catchUndefined(ignore);
-			avatarNode.get(function(avatarNode) {
-				$(".media-object", item).attr("src", avatarNode.value());
-			});
-			
-			moderation = moderations.select(session.node(post));
-			moderation.catchUndefined(function() {
-				$(".markAsRemarkable", item).removeAttr("disabled");
-			});
-			moderation.get(function(moderationNode) {
-				if (moderationNode.children().contains(aRemarkablePost).valueOf()) {
-					
-					$(".remarkablePostMarker", item).removeClass("hide");
-					$(".remarkablePostMarker", item).attr("style", "");
-					$(".markAsRemarkable", item).attr("disabled", "disabled");
-				} else {
-					console.log("dont highliught");
-					$(".markAsRemarkable", item).removeAttr("disabled");
-				}
-				
-			});
-
-			
-			$(".markAsRemarkable", item).click(function(evt) {
-				evt.preventDefault();
-				
-				var remarkableEntry = moderations.append("Remarkable");
-				remarkableEntry.append(aRemarkablePost);
-				remarkableEntry.append(session.node(post));
-				
-				
-				session.commit().get(function() { demo.update(); });
-			});
-			
-		};
-
-		demo.initUi = function() {
-
-			demo.update();
-
-			wrapper.show();
-		};
-
-		demo.createItem = function() {
-			var postContent;
-			postContent = $(".postTemplate", wrapper).clone();
-			postContent.removeClass("hide");
-			postContent.removeClass("postTemplate");
-			return postContent.appendTo($(postWrapper));
-		};
-
-		// resolving request for data node
-		posts.get(function(posts) {
-			// when data node loaded, show ui
-			demo.initUi();
-			wrapper.append($("<p>Loaded " + posts.uri() + "</p>"));
-
-			// installing monitor to check for updates from other clients
-			monitor = posts.monitor().setInterval("1000").setDepth(2)
-					.addListener(function(context) {
-						demo.update();
-					});
-
-			monitor.get(function(monitor) {
-
-			});
-
-			monitorModerations = moderations.monitor().setInterval("1000").setDepth(1).addListener(function(context) {
-				demo.update();
-			});
-			
-			monitorModerations.get(function(monitor) {
-				
-			});
-		});
-
 		return {
 			wrapper : wrapper,
 			shutdown : function() {
-				if (monitor !== null && monitor.get() !== null) {
-					monitor.get().stop();
-				}
-				session.close().get(function() {
-					server.shutdown.get(function() {
-					});
-				});
+				
 			}
 		};
 
